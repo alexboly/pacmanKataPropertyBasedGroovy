03928e46f433450acd53a9c9aa57df355c36a777
Alexandru Bolboaca <alexboly@gmail.com>
2017-09-02 16:28:53 +0300

    New feature: pacman eats dots to the left when in the middle and oriented towards left

--- src/test/groovy/PacmanSpec.groovy
+++ src/test/groovy/PacmanSpec.groovy
@@ -7,24 +7,9 @@ class PacmanSpec extends Specification {
 	final static pacmanTokenFacingRight = ">"
 	final static pacmanTokenFacingLeft = "<"
 
-	def "given a line of #dotsCount dots with pacman on the left oriented towards right, pacman eats the next dot on the right"() {
-		given: "a line of dots with pacman on the left oriented towards right"
-		def initialBoard = pacmanTokenFacingRight + lineOfDots(dotsCount)
-		def expectedFinalBoard = " " + pacmanTokenFacingRight + lineOfDots(dotsCount - 1)
-
-		when: "tick"
-		def boardAfterMove = tick(initialBoard, pacmanTokenFacingRight)
-
-		then: "the final board is"
-		boardAfterMove == expectedFinalBoard
-
-		where: "dots count"
-		dotsCount << (1..100)
-	}
-
-
-	def "given a line of #beforeDotsCount dots before pacman and #afterDotsCount after pacman oriented towards right, pacman eats the next dot on the right"() {
-		given: "a line of dots with pacman on the left oriented towards right"
+	@Unroll
+	def "pacman eats the next dot on the right when it's placed after #beforeDotsCount dots and has #afterDotsCount after it and it's oriented towards right"() {
+		given: "a line of dots with pacman in the middle oriented towards right"
 		def initialBoard = lineOfDots(beforeDotsCount) + pacmanTokenFacingRight + lineOfDots(afterDotsCount)
 		def expectedFinalBoard = lineOfDots(beforeDotsCount) + " " + pacmanTokenFacingRight + lineOfDots(afterDotsCount - 1)
 
@@ -35,14 +20,15 @@ class PacmanSpec extends Specification {
 		boardAfterMove == expectedFinalBoard
 
 		where: "dots count"
-		beforeDotsCount << (1..50)
-		afterDotsCount << Gen.integer(1..50).take(50)
+		beforeDotsCount << (0..<50)
+		afterDotsCount << Gen.integer(1..100).take(50)
 	}
 
-	def "given a line of #dotsCount dots with pacman on the right oriented towards left, pacman eats the next dot on the left"() {
-		given: "a line of dots with pacman on the right oriented towards left"
-		def initialBoard = lineOfDots(dotsCount) + pacmanTokenFacingLeft
-		def expectedFinalBoard = lineOfDots(dotsCount - 1) + pacmanTokenFacingLeft + " "
+	@Unroll
+	def "pacman eats the next dot on the left when it's placed after #beforeDotsCount dots and has #afterDotsCount after it and it's oriented towards left"() {
+		given: "a line of dots with pacman oriented towards left"
+		def initialBoard = lineOfDots(beforeDotsCount) + pacmanTokenFacingLeft + lineOfDots(afterDotsCount)
+		def expectedFinalBoard = lineOfDots(beforeDotsCount - 1) + pacmanTokenFacingLeft + " " + lineOfDots(afterDotsCount)
 
 		when: "tick"
 		def boardAfterMove = tick(initialBoard, pacmanTokenFacingLeft)
@@ -51,7 +37,8 @@ class PacmanSpec extends Specification {
 		boardAfterMove == expectedFinalBoard
 
 		where: "dots count"
-		dotsCount << (1..100)
+		beforeDotsCount << (1..50)
+		afterDotsCount << Gen.integer(1..100).take(50)
 	}
 
 	private lineOfDots(int dotsCount) {
