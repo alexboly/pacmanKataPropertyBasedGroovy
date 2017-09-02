6763fa5ba72ee56659021c9d5aaff7a0a1e549e5
Alexandru Bolboaca <alexboly@gmail.com>
2017-09-02 14:13:56 +0300

    Implement pacman move to the left

--- src/test/groovy/PacmanSpec.groovy
+++ src/test/groovy/PacmanSpec.groovy
@@ -3,7 +3,8 @@ import spock.lang.*
 
 @Log4j
 class PacmanSpec extends Specification {
-	private String pacmanTokenFacingRight = '>'
+	def pacmanTokenFacingRight = '>'
+	def pacmanTokenFacingLeft = '<'
 
 	@Unroll
 	def "given a line of #dotsCount dots with pacman on the left oriented towards right, pacman eats the next dot on the right"() {
@@ -21,20 +22,48 @@ class PacmanSpec extends Specification {
 		dotsCount << (1..100)
 	}
 
+	@Unroll
+	def "given a line of #dotsCount dots with pacman on the right oriented towards left, pacman eats the next dot on the left"() {
+		given: "a line of dots with pacman on the right oriented towards left"
+		def initialBoard = lineOfDots(dotsCount) + pacmanTokenFacingLeft
+		def expectedFinalBoard = lineOfDots(dotsCount - 1) + pacmanTokenFacingLeft + ' '
+
+		when: "tick"
+		def boardAfterMove = tick(initialBoard, pacmanTokenFacingLeft)
+
+		then: "the final board is"
+		boardAfterMove == expectedFinalBoard
+
+		where: "dots count"
+		dotsCount << (1..100)
+	}
+
 	private lineOfDots(int dotsCount) {
 		return (1..<dotsCount + 1).collect { '.' }.join('')
 	}
 
 	def tick(final board, final pacmanToken) {
+		if (pacmanToken == pacmanTokenFacingRight) {
+			return boardAfterPacmanMovedToNexPosition(
+					boardAfterPacmanMovedFromCurrentPosition(board, pacmanToken),
+					positionAfterPacmanMovesRight(board, pacmanToken),
+					pacmanToken
+			).join("")
+		}
+
 		return boardAfterPacmanMovedToNexPosition(
 				boardAfterPacmanMovedFromCurrentPosition(board, pacmanToken),
-				positionAfterPacmanMovesRight(board, pacmanToken),
+				positionAfterPacmanMovesLeft(board, pacmanToken),
 				pacmanToken
 		).join("")
 	}
 
 	static positionAfterPacmanMovesRight(final board, final pacmanToken) {
 		return board.indexOf(pacmanToken) + 1
+	}
+
+	static positionAfterPacmanMovesLeft(final board, final pacmanToken) {
+		return board.indexOf(pacmanToken) - 1
 	}
 
 	static boardAfterPacmanMovedFromCurrentPosition(final board, final pacmanToken) {
