94f6572ec87db84e3dad2145e5345ecb524904a9
Alexandru Bolboaca <alexboly@gmail.com>
2017-09-02 16:22:39 +0300

    New feature: pacman in the middle oriented towards right

--- src/test/groovy/PacmanSpec.groovy
+++ src/test/groovy/PacmanSpec.groovy
@@ -1,4 +1,5 @@
 import groovy.util.logging.Log4j
+import spock.genesis.Gen
 import spock.lang.*
 
 @Log4j
@@ -6,7 +7,6 @@ class PacmanSpec extends Specification {
 	final static pacmanTokenFacingRight = ">"
 	final static pacmanTokenFacingLeft = "<"
 
-	@Unroll
 	def "given a line of #dotsCount dots with pacman on the left oriented towards right, pacman eats the next dot on the right"() {
 		given: "a line of dots with pacman on the left oriented towards right"
 		def initialBoard = pacmanTokenFacingRight + lineOfDots(dotsCount)
@@ -22,7 +22,23 @@ class PacmanSpec extends Specification {
 		dotsCount << (1..100)
 	}
 
-	@Unroll
+
+	def "given a line of #beforeDotsCount dots before pacman and #afterDotsCount after pacman oriented towards right, pacman eats the next dot on the right"() {
+		given: "a line of dots with pacman on the left oriented towards right"
+		def initialBoard = lineOfDots(beforeDotsCount) + pacmanTokenFacingRight + lineOfDots(afterDotsCount)
+		def expectedFinalBoard = lineOfDots(beforeDotsCount) + " " + pacmanTokenFacingRight + lineOfDots(afterDotsCount - 1)
+
+		when: "tick"
+		def boardAfterMove = tick(initialBoard, pacmanTokenFacingRight)
+
+		then: "the final board is"
+		boardAfterMove == expectedFinalBoard
+
+		where: "dots count"
+		beforeDotsCount << (1..50)
+		afterDotsCount << Gen.integer(1..50).take(50)
+	}
+
 	def "given a line of #dotsCount dots with pacman on the right oriented towards left, pacman eats the next dot on the left"() {
 		given: "a line of dots with pacman on the right oriented towards left"
 		def initialBoard = lineOfDots(dotsCount) + pacmanTokenFacingLeft
