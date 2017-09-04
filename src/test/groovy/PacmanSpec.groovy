2c8346995987b48ae37b72d261e3df846ac82b66
Alexandru Bolboaca <alexboly@gmail.com>
2017-09-04 19:54:56 +0300

    Move duplication closer. Implement pacman move up

--- src/test/groovy/PacmanSpec.groovy
+++ src/test/groovy/PacmanSpec.groovy
@@ -7,6 +7,7 @@ class PacmanSpec extends Specification {
 	final static pacmanTokenFacingRight = ">"
 	final static pacmanTokenFacingLeft = "<"
 	final static pacmanTokenFacingDown = "v"
+	final static pacmanTokenFacingUp = "^"
 	final static emptySpace = " "
 	final static dot = "."
 	final static emptyPartialLine = []
@@ -16,7 +17,8 @@ class PacmanSpec extends Specification {
 		Dot,
 		PacmanLeft,
 		PacmanRight,
-		PacmanDown
+		PacmanDown,
+		PacmanUp
 
 		@Override
 		String toString() {
@@ -24,6 +26,7 @@ class PacmanSpec extends Specification {
 			if (this == PacmanLeft) return pacmanTokenFacingLeft
 			if (this == PacmanRight) return pacmanTokenFacingRight
 			if (this == PacmanDown) return pacmanTokenFacingDown
+			if (this == PacmanUp) return pacmanTokenFacingUp
 			if (this == Empty) return emptySpace
 			return ""
 		}
@@ -76,12 +79,25 @@ class PacmanSpec extends Specification {
 		boardAfterMove == expectedFinalBoard
 
 		where: "dots count"
-		beforeDotsCount = 1
-		afterDotsCount = 1
-//		beforeDotsCount << (0..<50)
-//		afterDotsCount << Gen.integer(1..100).take(50)
+		beforeDotsCount << (0..<50)
+		afterDotsCount << Gen.integer(1..100).take(50)
 	}
 
+	def "pacman eats the next dot up when it has dots up and is oriented up"() {
+		given: "a line of dots with pacman in the middle oriented towards up"
+		def initialBoard = columnOfDots(beforeDotsCount) + [[KindOfToken.PacmanUp]] + columnOfDots(afterDotsCount)
+		def expectedFinalBoard = columnOfDots(beforeDotsCount - 1) + [[KindOfToken.PacmanUp]] + [[KindOfToken.Empty]] + columnOfDots(afterDotsCount)
+
+		when: "tick"
+		def boardAfterMove = tick(initialBoard)
+
+		then: "the final board is"
+		boardAfterMove == expectedFinalBoard
+
+		where: "dots count"
+		beforeDotsCount << (1..50)
+		afterDotsCount << Gen.integer(1..100).take(50)
+	}
 
 	def "pacman eats the next dot on the left when it has dots on the left and it's oriented towards left"() {
 		given: "a line of dots with pacman oriented towards left"
@@ -124,43 +140,39 @@ class PacmanSpec extends Specification {
 
 	def tick(final board) {
 		def newBoard = null
+		def result
+		def finalToken
 
 		def line = board.first()
 		def possiblePacmanTokensForLine = [KindOfToken.PacmanLeft, KindOfToken.PacmanRight]
 		def intersection = line.intersect(possiblePacmanTokensForLine)
 		def existingToken = intersection ? intersection.first() : null
+
 		if (existingToken){
-			def newLine = computeNewLine(line, existingToken)
+			def before = beforeToken(line, existingToken)
+			def after = afterToken(line, existingToken)
+			if (existingToken == KindOfToken.PacmanLeft) result = computeNewBeforeAndNewAfterOnMoveLeft(before, after)
+			if (existingToken == KindOfToken.PacmanRight) result = computeNewBeforeAndNewAfterOnMoveRight(before, after)
+			finalToken = existingToken
+			def newLine = result.before + finalToken + result.after
 			newBoard = [newLine]
 		}
 
 		if (!existingToken) {
 			def column = board.collect{it.first()}
-			def possiblePacmanTokensForColumn = [KindOfToken.PacmanDown]
+			def possiblePacmanTokensForColumn = [KindOfToken.PacmanDown, KindOfToken.PacmanUp]
 			def columnIntersection = column.intersect(possiblePacmanTokensForColumn)
 			existingToken = columnIntersection ? columnIntersection.first() : null
 
-			def newColumn = computeNewColumn(column, KindOfToken.PacmanDown)
+			def before = beforeToken(column, existingToken)
+			def after = afterToken(column, existingToken)
+			if (existingToken == KindOfToken.PacmanDown) result = computeNewBeforeAndNewAfterOnMoveRight(before, after)
+			if (existingToken == KindOfToken.PacmanUp) result = computeNewBeforeAndNewAfterOnMoveLeft(before, after)
+			finalToken = [existingToken]
+			def newColumn = result.before + finalToken + result.after
 			newBoard = newColumn.collect{[it]}
 		}
-
-		assert existingToken
-
 		return newBoard
-	}
-
-	private computeNewLine(line, existingToken) {
-		def before = beforeToken(line, existingToken)
-		def after = afterToken(line, existingToken)
-		def result = computeNewBeforeAndNewAfter(before, after, existingToken)
-		return result.before + existingToken + result.after
-	}
-
-	private computeNewColumn(column, existingToken) {
-		def before = beforeToken(column, existingToken)
-		def after = afterToken(column, existingToken)
-		def result = computeNewBeforeAndNewAfter(before, after, existingToken)
-		return result.before + [existingToken] + result.after
 	}
 
 	def beforeToken(line, token) {
@@ -173,21 +185,6 @@ class PacmanSpec extends Specification {
 		def tokenIndex = line.findIndexOf { it == token }
 		def afterSubLineTokenCount = line.size() - tokenIndex - 1
 		return line.takeRight(afterSubLineTokenCount)
-	}
-
-	private computeNewBeforeAndNewAfter(before, after, pacmanToken) {
-		if (pacmanToken == KindOfToken.PacmanLeft) {
-			return computeNewBeforeAndNewAfterOnMoveLeft(before, after)
-		}
-
-		if (pacmanToken == KindOfToken.PacmanRight) {
-			return computeNewBeforeAndNewAfterOnMoveRight(before, after)
-		}
-
-		if (pacmanToken == KindOfToken.PacmanDown){
-			return computeNewBeforeAndNewAfterOnMoveRight(before, after)
-		}
-		return [[], []]
 	}
 
 	def computeNewBeforeAndNewAfterOnMoveRight(before, after) {
